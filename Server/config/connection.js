9a3d030526539c7b4fb9c2e05fb4a12249653031
Paul Baumert <baumert.trey@gmail.com>
2024-09-19 07:27:42 -0500

    added logic to connection, worked on building and running servers

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bbq_delivery', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
